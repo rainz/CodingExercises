84ff447331449a129c16dc5494ee9261232cf3ef
Y Z <rainzforever@gmail.com>
2020-01-07 23:02:00 -0500

    Create test.cpp

---
+++ ProblemsCPP/src/test.cpp
@@ -0,0 +1,105 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <map>
+#include <set>
+#include <algorithm> 
+#include <functional>
+#include "utils.h"
+
+using namespace std;
+
+struct TestObj {
+    int aField;
+    int bField;
+    TestObj(int a, int b) : aField(a), bField(b) {}
+};
+
+bool lessMod(int x, int y) {
+    return x % 3 < y % 3;
+}
+
+template <int MOD>
+struct MyFunctor {
+public:
+    bool operator()(int x, int y) {
+        return x % MOD < y % MOD;
+    }
+};
+
+struct MyCmpFunctor {
+public:
+    bool operator()(const TestObj& x, const TestObj& y) {
+        return x.bField < y.bField;
+    }
+};
+
+void testSorting()
+{
+    // Note: without a comparator, sort uses "<" operator.
+    // So you can override the "<" operator of a class
+
+    MyFunctor<3> ftr;
+    int arr[] = {9,8,7,6,5,14,13,12,11,10};
+    int modVal = 5;
+    auto cmp = [](auto x, auto y){ return x.aField < y.aField; };
+
+    // Array sorting
+    // sort(arr, arr+3); // sorts first 3
+    // sort(begin(arr), end(arr)); // entire array sorted
+    // sort(begin(arr), end(arr), greater<int>()); // sorted in reverse. Note the "()" after greater<int>
+    // sort(rbegin(arr), rend(arr)); // sorted in reverse
+    // sort(rbegin(arr), rbegin(arr)+6); // *last* 6 sorted in reverse
+    // sort(begin(arr), begin(arr)+4); // sorts firt 4
+    // sort(begin(arr), end(arr), lessMod); // sort by function
+    // sort(begin(arr), end(arr), ftr); // sort by functor
+    // sort(begin(arr), end(arr), [](auto x, auto y){return x % 5 < y % 5;}); // sort by lambda
+    // sort(begin(arr), end(arr), [&](auto x, auto y){return x % modVal < y % modVal;}); // sort by lambda with capture by ref
+    // sort(begin(arr), end(arr), [=](auto x, auto y){return x % modVal < y % modVal;}); // sort by lambda with capture by val
+    // printArray(arr, sizeof(arr)/sizeof(arr[0]));
+
+    // Vector sorting: similar to array sorting, use v.begin()/v.end() instead of begin(arr)/end(arr)
+    // vector<int> v(begin(arr), end(arr));
+    //sort(v.begin(), v.begin()+4, ftr); // sort first 4 with functor
+    // printVector(v);
+
+    // Map with comparators
+    // map<TestObj, int, MyCmpFunctor> treeMap; // using functor, can't use function
+    // map<TestObj, int, decltype(cmp)> treeMap(cmp); // using lambda
+    // for (int i: arr) {
+    //     TestObj obj(i, 100-i);
+    //     treeMap[obj] = i*i;
+    // }
+    // for (auto it = treeMap.cbegin(); it != treeMap.cend(); ++it)
+    //     cout << it->first.aField << "-" << it->first.bField << ": " << it->second << endl;
+
+    // Set with comparators
+    // set<TestObj, MyCmpFunctor> treeSet; // using functor, can't use function
+    // set<TestObj, decltype(cmp)> treeSet(cmp); // using lambda
+    // for (int i: arr) {
+    //     TestObj obj(i, 100-i);
+    //     treeSet.insert(obj);
+    // }
+    // for (auto it = treeSet.cbegin(); it != treeSet.cend(); ++it)
+    //     cout << it->aField << "-" << it->bField << endl;
+
+    // PQ with comparators
+    // priority_queue<TestObj, vector<TestObj>, MyCmpFunctor> pq; // using functor
+    // priority_queue<TestObj, vector<TestObj>, decltype(cmp)> pq(cmp); // using lambda
+    // for (int i: arr) {
+    //     TestObj obj(i, 100-i);
+    //     pq.push(obj);
+    // }
+    // while (!pq.empty()) {
+    //     auto &x = pq.top();
+    //     cout << x.aField << "-" << x.bField << endl;
+    //     pq.pop();
+    // }
+
+}
+
+int main(void)
+{
+    testSorting();
+    return 0;
+}
